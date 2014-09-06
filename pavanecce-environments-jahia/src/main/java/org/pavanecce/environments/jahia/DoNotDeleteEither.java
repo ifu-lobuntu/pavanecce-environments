7ef6a6cf99aee63d7f46cf5a01cace7a08c379f1
Ampie Barnard <ampieb@gmail.com>
2014-09-06 18:13:40 +0200

    Fixed bundle's pom

--- pavanecce-environments-jahia/src/main/java/org/pavanecce/environments/jahia/DoNotDeleteEither.java
+++ pavanecce-environments-jahia/src/main/java/org/pavanecce/environments/jahia/DoNotDeleteEither.java
@@ -1,5 +1,11 @@
 package org.pavanecce.environments.jahia;
 
+import javax.inject.Inject;
+
 public class DoNotDeleteEither {
+	@Inject
 	javax.enterprise.event.Event<?> event;
+//	net.sf.cglib.asm.signature s;
+	org.hibernate.annotations.common.AssertionFailure a;
+	
 }
